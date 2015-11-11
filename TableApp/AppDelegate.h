8c191841717aaed4e77ab18ff8fce6c7dc1a8e49
shahas <shahas@bullfin.ch>
2015-11-11 22:56:51 +0530

    Initial Commit

---
+++ TableApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableApp
+//
+//  Created by Shahas on 11/11/15.
+//  Copyright © 2015 Shahas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
